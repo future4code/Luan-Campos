281455cf2b777367c9da05e8137c72159756bc14
Luan C <luancnunes04@gmail.com>
2020-06-30 13:35:02 -0300

    Ex. 1

---
+++ BackEnd/semana19/Dia 2/jest-template/src/index.ts
@@ -0,0 +1,32 @@
+interface character {
+  name: string;
+  life: number;
+  str: number;
+  def: number;
+}
+
+const newChar: character = {
+  name: "Luan",
+  life: 1500,
+  str: 750,
+  def: 500,
+};
+
+function validateCharacter(char: character): Boolean {
+  if (
+    !char.name ||
+    char.life === undefined ||
+    char.str === undefined ||
+    char.def === undefined
+  ) {
+    return false;
+  }
+
+  if (char.life < 0 || char.str < 0 || char.def < 0) {
+    return false;
+  }
+
+  return true;
+}
+
+console.log(validateCharacter(newChar))
